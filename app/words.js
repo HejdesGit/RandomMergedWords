be39d95e1f53befa76264b3fb156835ab632106f
HejdesGit <hejdes@gmail.com>
2014-11-02 19:20:15 +0100

    Broken out words to app. Cleaned up module.

---
+++ app/words.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var Words = require('../words.json');
+
+module.exports = function words(options) {
+    function word1() {
+        return Words.wordList1[randInt(Words.wordList1.length)];
+    }
+
+    function word2() {
+        return Words.wordList2[randInt(Words.wordList2.length)];
+    }
+
+    function word3() {
+        return Words.wordList3[randInt(Words.wordList3.length)];
+    }
+
+    function randInt(lessThan) {
+        return Math.floor(Math.random() * lessThan);
+    }
+
+    // No arguments = generate one word
+    if (typeof(options) === 'undefined') {
+        return word1();
+    }
+
+    // Just a number = return that many words
+    if (typeof(options) === 'number') {
+        options = {exactly: options};
+    }
+
+    // options supported: exactly, min, max, join
+
+    if (options.exactly) {
+        options.min = options.exactly;
+        options.max = options.exactly;
+    }
+    var total = options.min + randInt(options.max + 1 - options.min);
+    var results = [];
+    var j = 1;
+    for (var i = 0; (i < total); i++) {
+        if (j === 1) {
+            results.push(word1())
+            j++;
+        }
+        else if (j === 2) {
+            results.push(word2());
+            j++;
+        }
+        else {
+            results.push(word3());
+            j = 1;
+        }
+    }
+    if (options.join === '') {
+        results = results.join('');
+    } else if (options.join) {
+        results = results.join(options.join);
+    }
+    return results;
+};
--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 var express = require('express'); 		// call express
 var app = express(); 				// define our app using express
 var bodyParser = require('body-parser');
-var randomWords = require('random-words');
+var randomWords = require('./app/words');
 var cors = require('cors');
 
 // configure app to use bodyParser()
