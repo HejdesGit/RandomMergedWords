7baba9ac783ef5d49453c5bf12856eca6895eb38
HejdesGit <hejdes@gmail.com>
2014-11-06 22:14:24 +0100

    Firebase implemented. Swaggy implemented.

--- server.js
+++ server.js
@@ -11,6 +11,57 @@ randomWords = require('./app/words'),
 cors = require('cors'),
 dataTalker = require('./app/data.talker'),
 swaggy = require("swaggy");
+var winston = require('winston'),
+    Papertrail = require('winston-papertrail').Papertrail;
+
+var logger,
+    consoleLogger = new winston.transports.Console({
+        level: 'debug',
+        timestamp: function() {
+            return new Date().toString();
+        },
+        colorize: true
+    }),
+    ptTransport = new Papertrail({
+        host: 'logs.papertrailapp.com',
+        port: 12345,
+        hostname: 'web-01',
+        level: 'debug',
+        colorize: true,
+        logFormat: function(level, message) {
+            return '[' + level + '] ' + message;
+        }
+    });
+
+ptTransport.on('error', function(err) {
+    logger && logger.error(err);
+});
+
+ptTransport.on('connect', function(message) {
+    logger && logger.info(message);
+});
+
+var logger = new winston.Logger({
+    levels: {
+        debug: 0,
+        info: 1,
+        warn: 2,
+        error: 3
+    },
+    colors: {
+        debug: 'blue',
+        info: 'green',
+        warn: 'red',
+        error: 'red',
+        auth: 'red'
+    },
+    transports: [
+        ptTransport,
+        consoleLogger
+    ]
+});
+
+logger.info('this is my message ' + new Date().getTime());
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
